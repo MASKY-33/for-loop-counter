e8a61f139f23a8dbad536eac7e011ebaeeac0fe7
MASKY-33 <masky.somebody33@gmail.com>
2026-06-20 16:38:52 +0200

    Update for-loop-counter.py

--- for-loop-counter.py
+++ for-loop-counter.py
@@ -3,7 +3,6 @@
 # - starts counting from 1 and ends at 10
 # - prints from 1 to 10 by ignoring 11
 
-
 count = 0
 
 for numbers in range(1, 11):  ## Can use the range() method in this example by doing (1, 11), or example (1, 11, 1). Both give the exact same results, but (1, 11, 1) makes it just more clearer, but I'll keep it on (1, 11)
